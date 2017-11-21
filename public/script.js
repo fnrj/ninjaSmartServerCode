4f4afc15efbfc361b04c783fc3166936b81e94d1
Alexander Brooks <abrooks9944@email.arizona.edu>
2017-11-21 01:11:06 -0700

    Added basic registration form, some JQuery/JS for transitions and basics for form validation

--- public/script.js
+++ public/script.js
@@ -1,23 +1,51 @@
 function blurBackground(){
+	/*Adds gaussian blur to the background image. Cosmetically
+	nice for emphasizing things like forms.*/
 	var elmt = document.getElementById("background");
 	if(!elmt.classList.contains("blur")){
 		elmt.classList.add("blur");
 	}
 }
 
-function sharpenBackground(){
-	var elmt = document.getElementById("background");
-	if(elmt.classList.contains("blur")){
-		elmt.classList.remove("blur");
+
+function validateRegistration(){
+    /*
+     * Basic form validation for email & password. Criteria are described below.
+     *
+     * Email: <anything>@<anything>.<anything>, with the restriction that '@' occurs only once.
+     * Password: contains number(s), upper/lowercase chars, 12-14 characters
+     */
+
+	var emailPattern = /\S+@\S+\.\S+/ 
+	var $email = $('#registerEmail').val();
+	var $password = $('#registerPassword').val();
+    		invalid = "#ffb380";
+
+	//email validation 
+	var emailStatus = emailPattern.test($email) && ($email.lastIndexOf('@') == $email.indexOf('@'));
+
+	//password validation
+	var caseCheck = ($password == $password.toLowerCase());
+	var lenCheck = $password.length > 6 && $password.length < 14;
+	var numCheck = /\d/.test($password);
+	var passwordStatus = caseCheck && lenCheck && numCheck;
+
+	//only submit the form if everything is valid
+	if(!(emailStatus && passwordStatus)){
+		alert('Invalid username or password. Alex will add handling here later.')
 	}
-
 }
 
-function navbarLogout(){
-	
+$(document).ready(function(){
+	//hiding and showing divs for different panels
+    $("#buttonLink").click(function(){
+    	$(".loginPanel").slideToggle();
+    });
+    $("#registerLink").click(function(){
+    	$(".loginPanel").slideToggle();
+    });
 
-}
+    //form validation methods (email and password) for new users
+    $("#signup").submit(validateRegistration);
+});
 
-function navbarLogin(){
-
-}
