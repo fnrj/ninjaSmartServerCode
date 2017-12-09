59bd8b2f0493e6c6ece5a63dc01a276bee749a69
Alexander Brooks <abrooks9944@email.arizona.edu>
2017-12-09 07:42:07 -0700

    added super basic weather example ajax call to dash.js for Tucson (does have UV value)

--- public/dash.js
+++ public/dash.js
@@ -7,22 +7,24 @@
 
 
 
-function populateDashboard(){
-    // Query the user's devices. Session is stored on server side.
-
-    $.get("http://localhost:3000/users/profile", function(data, status){    
-        console.log(data);
-        $("#userData").append("<ul>");
-        $("#userData").append("<li>" + data.devices[0] + "</li>");            
-        $("#userData").append("<li>" + data.userEmail + "</li>");                    
-        $("#userData").append("</ul>");  
-    }, "json")
-    .fail(function(jqxhr) {
-        window.location.href = "http://localhost:3000/login.html#";
-    })   
+function retrieveWeather(){
+    // Retrieve weather data using operweathermap. Sample query for Tucson 
+    var apikey = 'cd21c8117d8d56151a87ce0a0d61c921';
+    var lat = 32.2217;
+    var lon = 110.9265;
+    var cnt = 5;
+    var start = 1498049953;
+    var end = 1498481991;
+    var endpoint = 'http://api.openweathermap.org/data/2.5/uvi?appid=' + apikey + '&lat=' + lat + '&lon=' + lon + '&cnt=' + cnt + '&start='+ start + '&end=' + end;
     
+    $.get(endpoint, function(data, status){
+        console.log(data);
+    })
+    
+
 }
 
 $(document).ready(function(){
+    retrieveWeather();
     populateDashboard();
 });
